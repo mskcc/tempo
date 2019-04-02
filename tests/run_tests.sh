0312da7767971e521f214a4c7f5cede57db9756d
Ivkovic <sinisa.ivkovic@gmail.com>
2019-04-02 15:22:42 -0400

    rewrite test_runner for better logging and simplify adding tests

--- tests/run_tests.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-./nextflow run make_bam_and_qc.nf --sample tests/test_make_bam_and_qc.tsv -profile test
---
+++ tests/test_runner.py
@@ -0,0 +1,35 @@
+import sys
+import json
+from subprocess import Popen, PIPE
+
+
+def execute_test(name, command):
+    print("Running test %s" % name)
+    print("Command: %s" % command)
+    proc = Popen(command, stdout=PIPE, universal_newlines=True)
+    for line in proc.stdout:
+        print(line, end='')
+    proc.stdout.close()
+    exit_code = proc.wait()
+    return exit_code
+
+
+def print_report(status):
+    REPORT = "\nTEST REPORT:\n------------------------------------------"
+    for test in status:
+        REPORT += "\n%s" % test
+    print(REPORT)
+
+
+if __name__ == '__main__':
+    test_file = sys.argv[1]
+    with open(test_file, 'r') as f:
+        tests = json.load(f)
+    report = []
+    for k, v in tests.items():
+        return_code = execute_test(k, v)
+        if return_code == 0:
+            report.append("PASSED: %s" % k)
+        else:
+            report.append("FAILED: %s" % k)
+    print_report(report)
