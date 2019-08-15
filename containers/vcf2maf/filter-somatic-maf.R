69a35f5bfa684ea3a2e8f7c2d954b387f618fe91
kpjonsson <philip.jonsson@gmail.com>
2019-08-15 10:41:56 -0400

    parameterize

--- containers/vcf2maf/filter-somatic-maf.R
+++ containers/vcf2maf/filter-somatic-maf.R
@@ -8,17 +8,49 @@
 suppressPackageStartupMessages({
     library(data.table)
     library(annotateMaf)
+    library(argparse)
 })
 
 args = commandArgs(TRUE)
 
 if (is.null(args) | length(args)<1) {
-    message("Usage: filter-somatic-maf.R input.maf output.prefix")
+    message('Run filter-somatic-maf.R --help for list of input arguments.')
     quit()
 }
 
-maf = args[1]
-output_prefix = args[2]
+parser = ArgumentParser(description = 'Flag and filter somatic variants in input MAF file, output is a filtered and unfiltered but filter-tagged MAF file.')
+parser$add_argument('-m', '--maf-file', required = TRUE,
+                    help = 'VEP-annotated MAF file')
+parser$add_argument('-o', '--output-prefix', required = TRUE,
+                    help = 'Prefix of output files')
+parser$add_argument('-tv', '--tumor-vaf', required = FALSE,
+                    default = 0.05, help = 'Tumor variant allele frequency cut-off [default %(default)s]')
+parser$add_argument('-td', '--tumor-depth', required = FALSE,
+                    default = 20, help = 'Tumor variant loci total depth cut-off [default %(default)s]')
+parser$add_argument('-tc', '--tumor-count', required = FALSE,
+                    default = 3, help = 'Tumor variant read support cut-off [default %(default)s]')                    
+parser$add_argument('-nd', '--normal-depth', required = FALSE,
+                    default = 10, help = 'Normal variant loci total depth cut-off [default %(default)s]')
+parser$add_argument('-nc', '--normal-count', required = FALSE,
+                    default = 3, help = 'Normal variant read support cut-off [default %(default)s]')
+parser$add_argument('-gaf', '--gnomad-allele-frequency', required = FALSE,
+                    default = 0.01, help = 'gnomAD allele frequency cut-off [default %(default)s]')
+parser$add_argument('-pon', '--normal-panel-count', required = FALSE,
+                    default = 10, help = 'Panel of normals count cut-off [default %(default)s]')
+                
+    
+args = parser$parse_args()
+
+maf = args$maf_file
+output_prefix = args$output_prefix
+
+tumor_vaf_cutoff = args$tumor_vaf
+tumor_depth_cutoff = args$tumor_depth
+tumor_readcount_cutoff = args$tumor_count
+normal_depth_cutoff = args$normal_depth
+normal_readcount_cutoff = args$normal_count
+gnomad_af_cutoff = args$gnomad_allele_frequency
+pon_cutoff = args$normal_panel_count
 
 add_tag = function(filter, tag) {
     ifelse(filter == 'PASS',
@@ -35,24 +67,25 @@ maf[, `:=` (t_var_freq = t_alt_count/(t_alt_count+t_ref_count),
             RepeatMasker = ifelse(is.na(RepeatMasker), '', RepeatMasker),
             gnomAD_FILTER = ifelse(is.na(gnomAD_FILTER), 0, 1),
             FILTER = ifelse(!Custom_filters %in% c(NA, ''), Custom_filters, FILTER),
-            alt_bias = t_alt_count_raw_fwd == 0 | t_alt_count_raw_rev == 0,
-            ref_bias = t_depth_raw_fwd == 0 | t_depth_raw_fwd == 0
+            alt_bias = t_depth_raw > 5 & (t_alt_count_raw_fwd == 0 | t_alt_count_raw_rev == 0),
+            ref_bias = t_depth_raw > 5 & (t_depth_raw_fwd == 0 | t_depth_raw_fwd == 0)
 )]
 
-maf[t_var_freq < .05, FILTER := add_tag(FILTER, 'low_vaf')]
-maf[t_depth < 20, FILTER := add_tag(FILTER, 'low_t_depth')]
-maf[n_depth < 10, FILTER := add_tag(FILTER, 'low_n_depth')]
-maf[t_alt_count < 3, FILTER := add_tag(FILTER, 'low_t_alt_count')]
-maf[n_alt_count > 3 | n_alt_count_raw > 3, FILTER := add_tag(FILTER, 'high_n_alt_count')]
+# Apply filters to FILTER column
+maf[t_var_freq < tumor_vaf_cutoff, FILTER := add_tag(FILTER, 'low_vaf')]
+maf[t_depth < tumor_depth_cutoff, FILTER := add_tag(FILTER, 'low_t_depth')]
+maf[t_alt_count < tumor_readcount_cutoff, FILTER := add_tag(FILTER, 'low_t_alt_count')]
+maf[n_depth < normal_depth_cutoff, FILTER := add_tag(FILTER, 'low_n_depth')]
+maf[n_alt_count > normal_readcount_cutoff | n_alt_count_raw > normal_readcount_cutoff, FILTER := add_tag(FILTER, 'high_n_alt_count')]
 maf[EncodeDacMapability != '', FILTER := add_tag(FILTER, 'mapability')]
 maf[RepeatMasker != '', FILTER := add_tag(FILTER, 'repeatmasker')]
 if ('non_cancer_AF_popmax' %in% names(maf)) {
-    maf[non_cancer_AF_popmax > .01, FILTER := add_tag(FILTER, 'high_gnomad_pop_af')]
+    maf[non_cancer_AF_popmax > gnomad_af_cutoff, FILTER := add_tag(FILTER, 'high_gnomad_pop_af')]
 } else if ('AF_popmax' %in% names(maf)) {
-    maf[AF_popmax > .01, FILTER := add_tag(FILTER, 'high_gnomad_pop_af')]
+    maf[AF_popmax > gnomad_af_cutoff, FILTER := add_tag(FILTER, 'high_gnomad_pop_af')]
 }
-maf[PoN >= 10, FILTER := add_tag(FILTER, 'PoN')]
-maf[MQ < 55 & Variant_Type != 'SNP', FILTER := add_tag(FILTER, 'low_mq')]
+maf[PoN >= pon_cutoff, FILTER := add_tag(FILTER, 'PoN')]
+maf[MQ < 55 & Variant_Type != 'SNP', FILTER := add_tag(FILTER, 'low_mapping_quality')]
 maf[(t_alt_count_raw > 10 & alt_bias & MuTect2 == 0) |
     (t_alt_count_raw > 10 & alt_bias & ref_bias & MQ < 40 & MuTect2 == 0), FILTER := add_tag(FILTER, 'strand_bias')]
 
