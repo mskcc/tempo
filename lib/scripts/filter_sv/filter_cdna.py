bf971288ef1146a4b387a4b1fa462490291ca6b1
Anne Marie Noronha <anoronh4@users.noreply.github.com>
2021-12-14 16:38:59 -0500

    add scripts for filtering bedpe files

---
+++ lib/scripts/filter_sv/filter_cdna.py
@@ -0,0 +1,78 @@
+import argparse, sys, os
+import numpy as np
+import pandas as pd
+import pybedtools
+from utils import *
+
+def get_artefact_calls(df):
+	"""
+	Find genes with at least 2 exon-exon junctions
+	Input requires SVTYPE, EXON, Hugo_Symbol
+	Input requires variant id, gene names, exon names, splice site type
+	remove any variant that doesn't involve two exons
+	remove any variant that doesn't have "donor" and "acceptor" 
+	remove any variant that doesn't have "donor" and "acceptor" in the correct orientation
+	Only genes with at least two splice variants will be considered. 
+	"""
+	df = df.groupby(["var_id","gene"]).filter(lambda x: len(set(x['exon'])) == 2 and len(set(x['ss_type'])) == 2)
+	#df = df.groupby(["var_id","gene"]).filter(lambda x: "donor" == sorted(zip(x['exon_number'],x['ss_type']), key = lambda t:t[1], reverse= True if x['strand'] == "-" else False)[0][1] )
+	df = df.groupby(["var_id","gene"]).filter(lambda x: is_orientation_correct(x))
+	df = df.groupby(["gene"]).filter(lambda x: x.size() > 1)
+	return df["var_id"].tolist()
+	
+	#df = df.drop_duplicates().groupby(["var_id","gene"], as_index = False).agg({'exon': lambda x: len(set(x)) })
+	#artefact_genes = df[df.exon > 3].gene.tolist()
+	#return df[df.gene.isin(artefact_genes)]["var_id"].tolist()
+
+def is_orientation_correct(x):
+	reverse_ = True if x.strand.tolist()[0] == "-" else False
+	zipped = sorted(zip(x['exon_number'],x['ss_type']), key = lambda t:t[0], reverse= reverse_)
+	if zipped[0][1] == "donor":
+		return True
+	else: return False
+
+def usage():
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--exon-junct', dest="regions", help = 'regions with exon junctions', required = True)
+	parser.add_argument('--bedpe', help = 'bedpe file', required = True)
+	parser.add_argument('--output',dest="outfile",help = 'output file' , required = True)
+    
+	return parser.parse_args()
+
+def main():
+	args = usage()
+
+	[meta_header, bedpe_header_list] = get_bedpe_header(args.bedpe)
+	svtype_col = bedpe_header_list.index("TYPE")
+
+	intersect = run_pair_to_bed(args.bedpe,args.regions,match_type="both")
+	exon_del_df = pd.DataFrame(columns=["var_id","exon","exon_number","gene","ss_type","strand"])
+	for i in intersect:
+		if show_value(i[svtype_col]) != "DEL":
+			continue
+		data_dict = dict()
+		data_dict["var_id"] = i[7]
+		data_dict["exon"] = i[ (len(bedpe_header_list) - 1) + 4 ].split(":")[0]
+		data_dict["exon_number"] = data_dict["exon"].split(".")[-1]
+		data_dict["gene"] = ".".join(data_dict["exon"].split(".")[:-1])
+		# acceptor or donor
+		data_dict["ss_type"] = i[ (len(bedpe_header_list) - 1) + 4 ].split(":")[1] 
+		data_dict["strand"] = i[ (len(bedpe_header_list) - 1) + 6 ]
+		exon_del_df = pd.concat([exon_del_df,pd.DataFrame.from_dict(data_dict)])
+
+	exon_del_df.to_csv("debug.tsv",sep="\t",header=True, index=False)
+
+	artefact_calls = get_artefact_calls(exon_del_df.drop_duplicates(keep='first'))
+
+	cdna_filter = pybedtools.BedTool(args.bedpe)
+	cdna_filter = cdna_filter.to_dataframe(header=None, comment="#")
+	cdna_filter.columns = bedpe_header_list
+	cdna_filter = cdna_filter.apply(lambda x: update_filter(x,"cdna") if x["ID"] in artefact_calls else x, axis=1)
+	
+	with open(args.outfile, "w") as fw:
+		fw.write("".join(meta_header))
+		
+	cdna_filter.to_csv(args.outfile, header=True, index=False, sep="\t", mode="a")
+
+if __name__ == "__main__":
+	main()
---
+++ lib/scripts/filter_sv/utils.py
@@ -0,0 +1,49 @@
+import argparse
+import numpy as np
+import pandas as pd
+import pybedtools
+def add_tag(val,tag ):
+	"""
+	update FILTER value 
+	"""
+	newTag = tag
+	if val not in [".",None,"PASS",np.nan]:
+		newTag = "{},{}".format(val,tag)
+	return newTag
+
+def update_filter(row, tag):
+	"""
+	return row with updated FILTER value 
+	"""
+	row["FILTER"] = add_tag(row["FILTER"], tag)
+	return row
+
+
+def run_pair_to_bed(bedpe,regions,match_type):
+	a = pybedtools.BedTool(bedpe)
+	b = pybedtools.BedTool(regions)
+	result = a.pair_to_bed(b, **{'type': match_type})
+	return result
+	#in_type = match_type
+	#out_type = "notboth" if match_type == "both" else "neither"
+	#intersect = a.pair_to_bed(b, **{'type': in_type})
+	#outersect = a.pair_to_bed(b,**{'type': out_type})
+	#return [intersect, outersect]
+
+def bedtool_to_df(bt,header_list):
+	df = bt.to_dataframe(header=None)
+	if df.shape[0] > 0:
+		df = df[df.columns[:len(header_list)]]
+		df.columns = header_list
+	else:
+		df = pd.DataFrame(columns = header_list)
+	df = df.astype({i:int for i in "START_A|END_A|START_B|END_B".split("|")})
+	return df
+
+def get_bedpe_header(bedpe):
+	with open(bedpe, 'r') as f:
+		x = f.readlines()
+	meta_header = [x[y] for y in range(len(x)) if x[y].startswith("##")]
+	header_idx = [y for y in range(len(x)) if x[y].startswith("#CHROM")][0]
+	bedpe_header_list = x[header_idx].rstrip().split("\t")
+	return [meta_header, bedpe_header_list]
