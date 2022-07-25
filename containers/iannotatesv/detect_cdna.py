75b7fb2c13e122b932c61f95c865ab304a632559
Anne Marie Noronha <anoronh4@users.noreply.github.com>
2022-07-25 13:24:33 -0400

    add description and authorship info for each python file

--- containers/iannotatesv/detect_cdna.py
+++ containers/iannotatesv/detect_cdna.py
@@ -1,3 +1,12 @@
+#!/usr/bin/env python
+
+"""Identify deletion rearrangements that may pertain to cDNA contaminants"""
+
+__author__  = "Anne Marie Noronha"
+__email__   = "noronhaa@mskcc.org"
+__version__ = "0.0.1"
+__status__  = "Dev"
+
 import argparse, sys, os
 import numpy as np
 import pandas as pd
